ca0c2b9f9cc16180bb2c3a2aa6b8dfe03201aef9
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2023-09-28 14:43:01 +0400

    Allow users to ignore the content directory

--- example/application.cr
+++ example/application.cr
@@ -1,26 +1,12 @@
 require "../src/tilo"
 require "./constants"
 
-# Log.setup(:debug)
-
-# GC.disable
-
-# # You can connect to the JavaScript engine for debugging and as such.
-# puts "JavaScript engine running at: #{Tilo::JavaScript::Engine.instance.path}"
-
-# builder = Tilo::Builder.new(content_directory: Constants::CONTENT_DIRECTORY)
-# builder.build
+Log.setup(:debug)
 
 GC.disable
 
-html = <<-HTML
-  <Application applicationId="com.tilo.application">
-    <Window title="Untitled Window">
-      <Label>Hello, World!</Label>
-    </Window>
-  </Application>
-HTML
+# You can connect to the JavaScript engine for debugging and as such.
+puts "JavaScript engine running at: #{Tilo::JavaScript::Engine.instance.path}"
 
-builder = Tilo::Builder.new
-# Make sure you point the path the correct way.
-builder.build_from_string(html)
+builder = Tilo::Builder.new(content_directory: Constants::CONTENT_DIRECTORY)
+builder.build
